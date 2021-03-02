22ec71e7c45d6ec12b2d613e9374e571fb15d53d
Kelvin Tobar <71950186+ktobar@users.noreply.github.com>
2021-03-01 19:05:48 -0500

    added card and cardlist component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component.jsx";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    );    
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
   }
 }
 
