4e1cca1a0cf5c5031c0de06e4b77aefa1834c6e2
Kelvin Tobar <71950186+ktobar@users.noreply.github.com>
2021-03-29 14:12:26 -0400

    added handleChange method and use .bind

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,16 +21,21 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) => 
+    const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
     return (
       <div className="App">
-        <SearchBox placeholder='search monster'
-        handleChange = {e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
